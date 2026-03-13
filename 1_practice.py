59baf1085e05d630571439f9c0bc3efecb37d3af
Jiyoung Lee <75259368+ez0130@users.noreply.github.com>
2026-03-13 15:13:44 -0500

    Update comments for mathematical model section

---
+++ 1_practice.py
@@ -0,0 +1,17 @@
+'''
+1.Scenario setup 
+#     Route(Nodes): Manufacture Factory - > Warehouse - > Retailer
+#     Goal: Minimize Transportation Cost
+#     Factories : 2 places ( Constraints : produce limit)
+#     Warehouse : 3 places ( Constraints : storage and pass capacity limit)
+#     Retailer : 5 places ( Fixed Demand )
+#     Cost : transport cost for each edge
+2. Mathmatical Model
+#    Decision Variable:
+#     X(i,j) : Package amount moving from Factory i to Storage j
+#     Y(j,k) : Package amount moving from Storage j to Retailer k
+#    Objective Function:
+#     Minimum 
+
+'''
+
